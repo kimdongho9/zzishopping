c400735ab341c8b720c21b22d4a562d9d2cd76ca
kimdongho9 <guzzi9393@gmail.com>
2024-03-29 02:12:30 +0900

    크기조정

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -7,7 +7,7 @@ const ProductDetail = () => {
     let {id} = useParams()
     const[product,setProduct]=useState(null);
     const getProductDetail = async () => {
-        let url = `http://localhost:5000/product/${id}`;
+        let url = `http://localhost:5000/products/${id}`;
         let response = await fetch(url);
         let data = await response.json();
         setProduct(data);
